849531809c10e1585ef346a5ed97bd7c2fcee22b
Senicar23 <43275102+Senicar23@users.noreply.github.com>
2018-09-14 17:20:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Uzmi nasumičan - random broj od 1 do 10
+var broj = Math.ceil(Math.random() * 10);  
+
+ var izbor = prompt("Pogodi broj između 1 i 10");
+ 
+ if (izbor == broj) {
+	 
+	 alert("Bravo, ti si genij!");
+	 
+ }
+ else
+ {
+	 
+	 alert("Netočno, traženi broj je: " + broj);
+	 
+ }
